bace12db78bc69c6f496bcbe1c9ccd1fcc506371
Sudhir Singh <sudhirk926235@gmail.com>
2026-09-12 16:49:38 +0530

    test: fix Robolectric sdk version for QrCodeParserTest and enhance hero banner

--- app/src/test/java/com/example/QrCodeParserTest.kt
+++ app/src/test/java/com/example/QrCodeParserTest.kt
@@ -6,8 +6,10 @@ import org.junit.Assert.assertNotNull
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(sdk = [34])
 class QrCodeParserTest {
 
     @Test
